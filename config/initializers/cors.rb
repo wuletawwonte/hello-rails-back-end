d240b046d9c8c013792ca8e2d3ae010a5e69a4a9
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-09-03 10:51:34 +0300

    Fix rubocop linter erros

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://example.com:80'
-    resource '*', headers: :any, methods: [:get, :post]
+    origins '*'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
